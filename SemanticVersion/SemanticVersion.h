1294ea0be69d7077895cfc3ad8d7f8305d4f681c
SAKAI, Atsushi <sakai.atsushi@gmail.com>
2016-03-06 22:52:35 +0900

    create project

---
+++ SemanticVersion/SemanticVersion.h
@@ -0,0 +1,19 @@
+//
+//  SemanticVersion.h
+//  SemanticVersion
+//
+//  Created by 酒井篤 on 2016/03/06.
+//  Copyright © 2016年 Atsushi Sakai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SemanticVersion.
+FOUNDATION_EXPORT double SemanticVersionVersionNumber;
+
+//! Project version string for SemanticVersion.
+FOUNDATION_EXPORT const unsigned char SemanticVersionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SemanticVersion/PublicHeader.h>
+
+
