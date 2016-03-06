8192e13b09aeca59cd30457e750397f7ade460d4
SAKAI, Atsushi <sakai.atsushi@gmail.com>
2016-03-06 23:12:56 +0900

    add build settings

---
+++ SemanticVersion/SemanticVersion.swift
@@ -0,0 +1,13 @@
+//
+//  SemanticVersion.swift
+//  SemanticVersion
+//
+//  Created by 酒井篤 on 2016/03/06.
+//  Copyright © 2016年 Atsushi Sakai. All rights reserved.
+//
+
+import Foundation
+
+class SemanticVersion {
+
+}
